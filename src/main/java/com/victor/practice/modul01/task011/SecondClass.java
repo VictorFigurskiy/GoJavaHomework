134c9d976add3b129cc6848388f4f33c8262adfb
Victor Figurskiy <victor.ea.digital@gmail.com>
2017-01-30 00:43:05 +0200

    First commit

---
+++ src/main/java/com/victor/practice/modul01/task011/SecondClass.java
@@ -0,0 +1,10 @@
+package com.victor.practice.modul01.task011;
+
+/**
+ * Created by Sonikb on 26.01.2017.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
